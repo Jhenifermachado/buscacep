6364e0561f2a1bc68fe65744ad909271a40d48a4
jhenifer <jhenifermachado211@gmail.com>
2022-09-20 09:20:31 -0300

    enter

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      resultado: {},
+    };
+  },
+  methods: {
+    async buscar() {
+      const url = "viacep.com.br/ws/${this.cep}/json/;";
+      this.resultado = await axios.get(url);
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Buscar Cep</h1>
+  <form @submit.prevent="buscar">
+    <input type="text" v-model="cep" placeholder="Digite CEP" />
+    <button type="submit">Buscar</button>
+  </form>
+</template>
